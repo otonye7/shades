04d57d59eb2c8c7729bae21d2b0c231b33bfc031
otonye <otonyeamie@gmail.com>
2021-07-04 17:59:07 +0100

    master

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,54 @@
 import { HeaderContainer } from './header.styles';
+import VoicemailIcon from '@material-ui/icons/Voicemail';
+import SearchIcon from '@material-ui/icons/Search';
+import PersonIcon from '@material-ui/icons/Person';
+import LocalGroceryStoreIcon from '@material-ui/icons/LocalGroceryStore';
 
 const Header = () => {
     return (
         <HeaderContainer>
-            <div>Header components</div>
+            <div className='container'>
+            <div className='logo-container'>
+                <VoicemailIcon />
+                <div className='logo-text'>
+                    <h2 className='samo-text'>S0H0QUEST</h2>
+                </div>
+            </div>
+
+            <div className='nav-links'>
+                <div className='navs'>
+                    <h5 className='nav-text'>Sunglasses</h5>
+                </div>
+                <div className='navs'>
+                    <h5 className='nav-text'>Sports</h5>
+                </div>
+                <div className='navs'>
+                    <h5 className='nav-text'>Bluelight</h5>
+                </div>
+                <div className='navs'>
+                    <h5 className='nav-text'>Accessories</h5>
+                </div>
+                <div className='navs'>
+                    <h5 className='nav-text'>Sales</h5>
+                </div>
+                <div className='navs'>
+                    <h5 className='nav-text'>Learnmore</h5>
+                </div>
+                </div>
+
+                <div className='header-icons'>
+                    <div className='searchicons'>
+                        <SearchIcon />
+                    </div>
+                    <div className='personicons'>
+                        <PersonIcon />
+                    </div>
+                    <div className='carticons'>
+                        <LocalGroceryStoreIcon />
+                    </div>
+            </div>
+            </div>
+            <div className='border-bottom'></div>
         </HeaderContainer>
         
     )
--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -4,4 +4,46 @@ import styled from 'styled-components';
 export const HeaderContainer = styled.div`
    width: 100%;
 
+
+   .container {
+    width: 90%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+   }
+  
+
+  .logo-container {
+      width: 15%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center; 
+  }
+
+  .nav-links {
+      display: flex;
+      width: 65%;
+      justify-content: space-between;
+  }
+
+  .nav-text {
+    font-size: 14px;
+    font-weight: 400;
+    text-transform: uppercase;
+    font-family: Lato,sans-serif;
+    font-style: normal;
+  }
+
+  .header-icons {
+      display: flex;
+      justify-content: space-between;
+      width: 8%;
+      align-content: center;
+      align-items: center;
+  }
+
+  .border-bottom {
+    border: 1px solid black;
+    opacity: 0.1;
+  }
 `;
