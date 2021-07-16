d7bd77c812f6e0759c9d89842cf786abc7e71ee1
otonye <otonyeamie@gmail.com>
2021-07-17 00:06:37 +0100

    master

--- src/components/card-one/card-one.component.jsx
+++ src/components/card-one/card-one.component.jsx
@@ -1,30 +1,23 @@
 import { CardContainer } from './card-one.styles';
-import Glass from  '../../assets/claudio.jpg';
-import {Link} from 'react-router-dom';
+import Glass from '../../assets/glass1.png';
+import GlassTwo from '../../assets/glass2.png';
+import GlassThird from '../../assets/glass3.png';
+
 
 const CardOne = () => {
     return (
         <CardContainer>
             <div className='leftimage'>
                 <img src={Glass} alt="" />
-                <div className='links'>
-                    <Link className='link-text'>Alpha Frame</Link> 
-                </div>
-             </div>
+            </div>
 
-             <div className='leftimage'>
-                <img src={Glass} alt="" />
-                <div className='links'>
-                    <Link className='link-text'>Alpha Frame</Link> 
-                </div>
-             </div>
+            <div className='leftimage'>
+                <img src={GlassTwo} alt="" />
+            </div>
 
-             <div className='leftimage'>
-                <img src={Glass} alt="" />
-                <div className='links'>
-                    <Link className='link-text'>Alpha Frame</Link> 
-                </div>
-             </div>
+            <div className='leftimage'>
+                <img src={GlassThird} alt="" />
+            </div>
         </CardContainer>
     )
 }
--- src/components/card-two/card-two.component.jsx
+++ src/components/card-two/card-two.component.jsx
@@ -1,30 +1,23 @@
 import { CardContainer } from './card-two.styles';
-import Glass from  '../../assets/claudio.jpg';
-import {Link} from 'react-router-dom';
+import Glass from '../../assets/glass6.png';
+import GlassFour from '../../assets/glass4.png';
+import GlassFive from '../../assets/glass5.png';
+
 
 const CardTwo = () => {
     return (
         <CardContainer>
             <div className='leftimage'>
                 <img src={Glass} alt="" />
-                <div className='links'>
-                    <Link className='link-text'>Alpha Frame</Link> 
-                </div>
-             </div>
+            </div>
 
-             <div className='leftimage'>
-                <img src={Glass} alt="" />
-                <div className='links'>
-                    <Link className='link-text'>Alpha Frame</Link> 
-                </div>
-             </div>
+            <div className='leftimage'>
+                <img src={GlassFour} alt="" />
+            </div>
 
-             <div className='leftimage'>
-                <img src={Glass} alt="" />
-                <div className='links'>
-                    <Link className='link-text'>Alpha Frame</Link> 
-                </div>
-             </div>
+            <div className='leftimage'>
+                <img src={GlassFive} alt="" />
+            </div>
         </CardContainer>
     )
 }
